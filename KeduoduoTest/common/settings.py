df90daed77da0f340515c084a0faba82316dc161
Sea-Monster <cristianofigo@126.com>
2017-10-18 11:00:05 +0800

    所有settings.py的配置都从配置文件中读取

--- KeduoduoTest/common/settings.py
+++ KeduoduoTest/common/settings.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import configparser
+import os
 
-cf = configparser.ConfigParser()
-cf.read('/Users/SeaMonster/PycharmProjects/KeduoduoTest/settings.conf')
-IMAGE_PATH = '/Users/SeaMonster/PycharmProjects/KeduoduoTest/images/'
-IMAGE_SUBFIX_DEFAULT = '.png'
-LOGS_PATH = '/Users/SeaMonster/PycharmProjects/KeduoduoTest/logs/'
-USER_NAME = cf.get('AUTH', 'user_name')
-PASSWORD = cf.get('AUTH', 'password')
-BASE_URL = cf.get('URL', 'base_url')
-BASE_LOGIN_URL = cf.get('URL', 'base_login_url')
-NO_PIC = False if cf.get('WEB_SETTING', 'no_pic') == '0' else True
+__all__= ['IMAGE_PATH','IMAGE_SUBFIX_DEFAULT','LOGS_PATH','USER_NAME','PASSWORD','BASE_URL','BASE_LOGIN_URL','NO_PIC']
+
+_cf = configparser.ConfigParser()
+_cf.read(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + '/settings.conf')
+IMAGE_PATH = _cf.get('OUTPUT', 'image_path')
+IMAGE_SUBFIX_DEFAULT = _cf.get('OUTPUT', 'image_default_subfix')
+LOGS_PATH = _cf.get('OUTPUT', 'logs_path')
+USER_NAME = _cf.get('AUTH', 'user_name')
+PASSWORD = _cf.get('AUTH', 'password')
+BASE_URL = _cf.get('URL', 'base_url')
+BASE_LOGIN_URL = _cf.get('URL', 'base_login_url')
+NO_PIC = False if _cf.get('WEB_SETTING', 'no_pic') == '0' else True
